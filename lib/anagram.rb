c8de1be848cb61dce6471daa4d13b10a31ec2afc
Garrett Thompson <gtcruz.gt@gmail.com>
2019-08-10 23:34:13 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,13 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :word, :test_string
+
+  def initialize(word)
+    @word = word.split(" ")
+  end
+
+  def match(test_string)
+    @test_string = test_string.split(" ")
+    return @test_string & @word
+  end
+end
